6459737869a09721bf7e23b4b6b9d2b590dbd415
Rachita Cosmin <rachitacosminv@gmail.com>
2021-01-04 13:14:48 +0200

    last update

--- main.py
+++ main.py
@@ -1,69 +1,116 @@
 from tinydb import TinyDB, Query
+from datetime import datetime
+
 import json
 import math
 db = TinyDB('db.json')
 User = Query()
 
-Inventar = {'Banane': '2', 'Mere': '6', 'Struguri': '5', 'Portocale': '7', 'Zile': '13'}
-Comanda1 = {'Banane': '40', 'Mere': '100', 'Struguri': '27', 'Portocale': '14', 'Zile': '13'}
-Comanda2 = {'Banane': '12', 'Mere': '70', 'Struguri': '40', 'Portocale': '40', 'Zile': '13'}
-Comanda3 = {'Banane': '0', 'Mere': '25', 'Struguri': '13', 'Portocale': '100', 'Zile': '9'}
+Inventar = {'Banane': '2', 'Mere': '6', 'Struguri': '5', 'Portocale': '7'}
+Comanda = {'Comanda1': {'data' : '2020-12-25T00:05:23','fruct' : 'Banane','cantitate' : 20},
+           'Comanda2': {'data' : '2020-12-24T00:05:23','fruct' : 'Struguri','cantitate' : 30},
+           'Comanda3': {'data' : '2020-12-18T00:05:23','fruct' : 'Mere','cantitate' : 50},
+           'Comanda4': {'data' : '2020-12-24T00:05:23','fruct' : 'Portocale','cantitate' : 27},
+           'Comanda5': {'data' : '2020-12-27T00:05:23','fruct' : 'Banane','cantitate' : 45}
+           }
 
 db.insert(Inventar)
-db.insert(Comanda1)
-db.insert(Comanda2)
-db.insert(Comanda3)
+db.insert(Comanda)
 Valori = db.all()
 
-
-def numar_marfa(Valori, zilele):
+def numar_marfa(Valori, TimeStamp, Zile):
     BList = []
     MList = []
     SList = []
     PList = []
-    ZList = []
     MaxBanane = 0
     MaxMere = 0
     MaxStruguri = 0
     MaxPortocale = 0
     Inventarul = []
+
+    TimeStamp = datetime.fromisoformat(TimeStamp)
+    perioada = datetime.now() - TimeStamp
+
     for key in Valori[0]:
         Inventarul.append(Valori[0][key])
-    for i in range(0, 4):
-        BList.append(Valori[i]["Banane"])
-        MList.append(Valori[i]["Mere"])
-        SList.append(Valori[i]["Struguri"])
-        PList.append(Valori[i]["Portocale"])
-        ZList.append(Valori[i]["Zile"])
-    print(BList)
-    print(MList)
-    print(SList)
-    print(PList)
-    print(ZList)
+    for element in Valori[1]:
+        for el in Valori[1][element]:
+            if (el == 'fruct' and Comanda[element][el] == 'Banane' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
+                BList.append(Valori[1][element]['cantitate'])
+            if (el == 'fruct' and Comanda[element][el] == 'Mere' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
+                MList.append(Valori[1][element]['cantitate'])
+            if (el == 'fruct' and Comanda[element][el] == 'Struguri' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
+                SList.append(Valori[1][element]['cantitate'])
+            if (el == 'fruct' and Comanda[element][el] == 'Portocale' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
+                PList.append(Valori[1][element]['cantitate'])
+    print("BList" + str(BList))
+    print("MList" + str(MList))
+    print("SList" + str(SList))
+    print("PList" + str(PList))
     print(Inventarul)
+    print (perioada)
+    for element in MList:
+        MaxMere = MaxMere + element
 
-    for i in range(0, 3):
-        MaxMere = max(MaxMere, math.ceil(int(MList[i]) / int(ZList[i])))
-        MaxBanane = max(MaxBanane, math.ceil(int(BList[i]) / int(ZList[i])))
-        MaxStruguri = max(MaxStruguri, math.ceil(int(SList[i]) / int(ZList[i])))
-        MaxPortocale = max(MaxPortocale, math.ceil(int(PList[i]) / int(ZList[i])))
+    for element in BList:
+        MaxBanane = MaxBanane + element
+
+    for element in SList:
+        MaxStruguri = MaxStruguri + element
+
+    for element in PList:
+        MaxPortocale = MaxPortocale + element
+
+    print("\n")
     print("nr de mere maxim " + str(MaxMere))
     print("nr de Banane maxim " + str(MaxBanane))
     print("nr de struguri maxim " + str(MaxStruguri))
     print("nr de portocale maxim " + str(MaxPortocale))
+    print("\n")
 
-    MaxBanane = MaxBanane * zilele
-    MaxMere = MaxMere * zilele
-    MaxStruguri = MaxStruguri * zilele
-    MaxPortocale = MaxPortocale * zilele
+    print( math.ceil(MaxBanane / perioada.days))
+    print( math.ceil(MaxMere / perioada.days))
+    print( math.ceil(MaxStruguri / perioada.days))
+    print( math.ceil(MaxPortocale / perioada.days))
 
-    print("comanda pentru urmatoarele " + str(zilele) + " zile este: \n" +  str(MaxBanane-int(Inventarul[0])) + " Banane \n" +
-          str(MaxMere-int(Inventarul[1])) + " Mere\n" + str(MaxStruguri-int(Inventarul[2])) + " Struguri\n" + str(MaxPortocale-int(Inventarul[3])) + " Portocale")
-    # print(produse)
-    # print(db.all())
+    MaxBanane = math.ceil(MaxBanane / perioada.days ) * Zile
+    MaxMere = math.ceil(MaxMere / perioada.days ) * Zile
+    MaxStruguri = math.ceil(MaxStruguri / perioada.days ) * Zile
+    MaxPortocale = math.ceil(MaxPortocale / perioada.days ) * Zile
+
+    print("nr de mere maxim " + str(MaxMere))
+    print("nr de Banane maxim " + str(MaxBanane))
+    print("nr de struguri maxim " + str(MaxStruguri))
+    print("nr de portocale maxim " + str(MaxPortocale))
+    print("\n")
+
+    if(MaxMere == 0):
+        CMere = 0
+    else:
+        CMere = MaxMere - int(Inventarul[1])
+
+    if(MaxBanane == 0):
+        CBanane = 0
+    else:
+        CBanane = MaxBanane - int(Inventarul[0])
+
+    if(MaxStruguri == 0):
+        CStruguri = 0
+    else:
+        CStruguri = MaxStruguri - int(Inventarul[2])
+
+    if(MaxPortocale == 0):
+        CPortocale = 0
+    else:
+        CPortocale = MaxPortocale - int(Inventarul[3])
+
+    print("comanda pentru urmatoarele " + str(Zile) + " zile este: \n" +  str(CBanane) + " Banane \n" +
+          str(CMere) + " Mere\n" + str(CStruguri) + " Struguri\n" + str(CPortocale) + " Portocale")
 
 def main():
-    numar_marfa(Valori,6)
-
+    numar_marfa(Valori,'2020-12-26T00:05:23',6)
+    aux =open('db.json','w')
+    aux.write('{}')
 if __name__ == "__main__":
     main()
