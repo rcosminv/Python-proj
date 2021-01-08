15ff2e8324a6ca17056a11d314871c55cad919f4
Rachita Cosmin <rachitacosminv@gmail.com>
2021-01-08 20:30:45 +0200

    yes

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ User = Query()
 
 Inventar = {'Banane': '2', 'Mere': '6', 'Struguri': '5', 'Portocale': '7'}
 Comanda = {'Comanda1': {'data' : '2020-12-25T00:05:23','fruct' : 'Banane','cantitate' : 20},
-           'Comanda2': {'data' : '2020-12-24T00:05:23','fruct' : 'Struguri','cantitate' : 30},
+           'Comanda2': {'data' : '2020-12-25T00:05:23','fruct' : 'Struguri','cantitate' : 30},
            'Comanda3': {'data' : '2020-12-18T00:05:23','fruct' : 'Mere','cantitate' : 50},
            'Comanda4': {'data' : '2020-12-24T00:05:23','fruct' : 'Portocale','cantitate' : 27},
-           'Comanda5': {'data' : '2020-12-27T00:05:23','fruct' : 'Banane','cantitate' : 45}
+           'Comanda5': {'data' : '2020-12-27T00:05:23','fruct' : 'Banane','cantitate' : 45},
+           'Comanda6': {'data' : '2020-12-27T00:05:23','fruct' : 'Banana2','cantitate' : 4}
            }
 
 db.insert(Inventar)
@@ -19,98 +20,86 @@ db.insert(Comanda)
 Valori = db.all()
 
 def numar_marfa(Valori, TimeStamp, Zile):
-    BList = []
-    MList = []
-    SList = []
-    PList = []
-    MaxBanane = 0
-    MaxMere = 0
-    MaxStruguri = 0
-    MaxPortocale = 0
-    Inventarul = []
 
-    TimeStamp = datetime.fromisoformat(TimeStamp)
     perioada = datetime.now() - TimeStamp
+    elemente = []
 
-    for key in Valori[0]:
-        Inventarul.append(Valori[0][key])
     for element in Valori[1]:
-        for el in Valori[1][element]:
-            if (el == 'fruct' and Comanda[element][el] == 'Banane' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
-                BList.append(Valori[1][element]['cantitate'])
-            if (el == 'fruct' and Comanda[element][el] == 'Mere' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
-                MList.append(Valori[1][element]['cantitate'])
-            if (el == 'fruct' and Comanda[element][el] == 'Struguri' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
-                SList.append(Valori[1][element]['cantitate'])
-            if (el == 'fruct' and Comanda[element][el] == 'Portocale' and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
-                PList.append(Valori[1][element]['cantitate'])
-    print("BList" + str(BList))
-    print("MList" + str(MList))
-    print("SList" + str(SList))
-    print("PList" + str(PList))
-    print(Inventarul)
+        for ala in Valori[1][element]:
+            if (ala == 'fruct' and Comanda[element][ala] not in elemente):
+                elemente.append(Comanda[element][ala])
+
+    valoriMaxime = [None] * len(elemente)
+
+    for element in Comanda:
+        for ala in Comanda[element]:
+            if (ala == 'fruct'):
+                nume = Comanda[element][ala]
+        for fructul in range(0, len(elemente)):
+            if (nume == elemente[fructul] and  TimeStamp < datetime.fromisoformat(Comanda[element]['data'])):
+                print(fructul)
+                if (valoriMaxime[fructul] == None):
+                    valoriMaxime[fructul] = Comanda[element]['cantitate']
+                else:
+                    valoriMaxime[fructul] += Comanda[element]['cantitate']
+
+    print(valoriMaxime)
     print (perioada)
-    for element in MList:
-        MaxMere = MaxMere + element
-
-    for element in BList:
-        MaxBanane = MaxBanane + element
-
-    for element in SList:
-        MaxStruguri = MaxStruguri + element
-
-    for element in PList:
-        MaxPortocale = MaxPortocale + element
 
     print("\n")
-    print("nr de mere maxim " + str(MaxMere))
-    print("nr de Banane maxim " + str(MaxBanane))
-    print("nr de struguri maxim " + str(MaxStruguri))
-    print("nr de portocale maxim " + str(MaxPortocale))
+    for iterator in range (0,len(valoriMaxime)):
+        print(str(elemente[iterator]) + " " + str(valoriMaxime[iterator]))
+
+    for iterator in range(0, len(valoriMaxime)):
+        if(valoriMaxime[iterator] == None):
+            valoriMaxime[iterator] = 0
+
+    for iterator in range (0,len(valoriMaxime)):
+        valoriMaxime[iterator] = math.ceil(valoriMaxime[iterator] / perioada.days) *Zile
+        print(valoriMaxime[iterator])
+
     print("\n")
 
-    print( math.ceil(MaxBanane / perioada.days))
-    print( math.ceil(MaxMere / perioada.days))
-    print( math.ceil(MaxStruguri / perioada.days))
-    print( math.ceil(MaxPortocale / perioada.days))
+    for iterator in range(0, len(valoriMaxime)):
+        if(valoriMaxime[iterator] == 0):
+            valoriMaxime[iterator] = 0
+        else:
+            if(elemente[iterator]  not in Inventar):
+                Inventar[elemente[iterator]] = 0
+            if(valoriMaxime[iterator] - int(Inventar[elemente[iterator]]) < 0):
+                valoriMaxime[iterator] = 0
+            else:
+                valoriMaxime[iterator]-= int(Inventar[elemente[iterator]])
 
-    MaxBanane = math.ceil(MaxBanane / perioada.days ) * Zile
-    MaxMere = math.ceil(MaxMere / perioada.days ) * Zile
-    MaxStruguri = math.ceil(MaxStruguri / perioada.days ) * Zile
-    MaxPortocale = math.ceil(MaxPortocale / perioada.days ) * Zile
+    print("comanda pentru urmatoarea perioada este:")
+    for iterator in range (0,len(valoriMaxime)):
+        print(str(elemente[iterator]) + " " + str(valoriMaxime[iterator]))
 
-    print("nr de mere maxim " + str(MaxMere))
-    print("nr de Banane maxim " + str(MaxBanane))
-    print("nr de struguri maxim " + str(MaxStruguri))
-    print("nr de portocale maxim " + str(MaxPortocale))
-    print("\n")
-
-    if(MaxMere == 0):
-        CMere = 0
-    else:
-        CMere = MaxMere - int(Inventarul[1])
-
-    if(MaxBanane == 0):
-        CBanane = 0
-    else:
-        CBanane = MaxBanane - int(Inventarul[0])
-
-    if(MaxStruguri == 0):
-        CStruguri = 0
-    else:
-        CStruguri = MaxStruguri - int(Inventarul[2])
-
-    if(MaxPortocale == 0):
-        CPortocale = 0
-    else:
-        CPortocale = MaxPortocale - int(Inventarul[3])
-
-    print("comanda pentru urmatoarele " + str(Zile) + " zile este: \n" +  str(CBanane) + " Banane \n" +
-          str(CMere) + " Mere\n" + str(CStruguri) + " Struguri\n" + str(CPortocale) + " Portocale")
 
 def main():
-    numar_marfa(Valori,'2020-12-26T00:05:23',6)
+    luna = 100
+    zile = 100
+    ora = 100
+    minut = 100
+    nrZile = 0
+    an = 0
+    while(nrZile < 1):
+        nrZile = int(input("introdu numarul de zile "))
+    while(an < 1):
+        an = int(input("introdu anul pentru timestamp "))
+    while(luna > 12 or luna <= 0):
+        luna = int(input("introdu luna pentru timestamp "))
+    while(zile > 31 or zile <= 0):
+        zile = int(input("introdu ziua pentru timestamp "))
+    while(ora > 24 or ora < 0):
+        ora = int(input("introdu ora pentru timestamp "))
+    while(minut < 0 or minut > 60):
+        minut = int(input("introdu minutele pentru timestamp "))
+    print(datetime(an, luna, zile, ora, minut, 0, 0 ))
+    timp = datetime(an, luna, zile, ora, minut, 0, 0 )
+    numar_marfa(Valori, timp ,int(nrZile))
     aux =open('db.json','w')
     aux.write('{}')
 if __name__ == "__main__":
     main()
+
